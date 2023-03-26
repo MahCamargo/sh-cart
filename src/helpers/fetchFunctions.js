a50a8ea4a9f67dfbb0e227caa25456d446b4ad81
marciacosta <mahcosta1109@gmail.com>
2023-03-26 18:12:15 -0300

    requisitos em testes

--- src/helpers/fetchFunctions.js
+++ src/helpers/fetchFunctions.js
@@ -1,7 +1,17 @@
 export const fetchProduct = () => {
-  // seu código aqui
+  if (typeof module !== 'undefined') {
+    module.exports = {
+      fetchProducts,
+    };
+  }
 };
 
-export const fetchProductsList = () => {
-  // seu código aqui
+export const fetchProductsList = async (product) => {
+  if (!product) { throw new Error('Termo de busca não informado'); }
+
+  const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${product}`);
+  const data = await response.json();
+  return data.results;
 };
+
+fetchProductsList();
