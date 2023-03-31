c1b13712653cf803ed5206940d56064de4bd54bc
marciacosta <mahcosta1109@gmail.com>
2023-03-31 00:26:59 -0300

    refazendo teste da 1

--- src/tests/fetchProductsList.test.js
+++ src/tests/fetchProductsList.test.js
@@ -1,39 +1,34 @@
 import './mocks/fetchSimulator';
 import { fetchProductsList } from '../helpers/fetchFunctions';
 import computadorSearch from './mocks/search';
-
 // implemente seus testes aqui
-const { fetchProductsList } = require('./fetchFunctions');
-
 describe('Teste a função fetchProductsList', () => {
   it('fetchProductsList é uma função', () => {
     expect(typeof fetchProductsList).toBe('function');
   });
 
-  it('fetch é chamado ao executar fetchProductsList', async () => {
-    global.fetch = jest.fn(() => Promise.resolve({
-      json: () => Promise.resolve({}),
-    }));
-
-    await fetchProductsList();
-
-    expect(fetch).toHaveBeenCalledTimes(1);
-
-    global.fetch.mockClear();
-    delete global.fetch;
+  it('testa fetchProductsList com o argumento computador se fetch foi chamada', async () => {
+    await fetchProductsList('computador');
+    expect(fetch).toHaveBeenCalledTimes(1)
   });
 
-  it('fetch é chamado com o endpoint correto ao executar fetchProductsList', async () => {
-    global.fetch = jest.fn(() => Promise.resolve({
-      json: () => Promise.resolve({}),
-    }));
-
-    await fetchProductsList();
-
-    expect(fetch).toHaveBeenCalledWith('https://exemplo.com/api/products');
-
-    global.fetch.mockClear();
-    delete global.fetch;
+  it('testa fetchProductsList com o argumento computador se fetch foi chamada com URL correto', async () => {
+    await fetchProductsList('computador');
+    expect(fetch).toHaveBeenCalledWith('https://api.mercadolibre.com/sites/MLB/search?q=computador')
   });
-});
 
+  it('testa fetchProductsList com o argumento computador retorna os dados de computadorSearch', async () => {
+    const result = await fetchProductsList('computador');
+    expect(result).toEqual(computadorSearch);
+  });
+
+  it('should return a error if is invalid action', async () => {
+    const result = await fetchProductsList()
+        expect(result).toBeInstanceOf(Error);
+        expect(result.message).toEqual('Termo de busca não informado');
+    })
+
+})
+
+
+  
